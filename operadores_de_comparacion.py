78a765aa24dc3b52113827f6a512fe674bcc8510
cherihosky wssehin <81282060+Cherihosky@users.noreply.github.com>
2024-07-24 19:22:39 -0500

    Add files via upload

---
+++ operadores_de_comparacion.py
@@ -0,0 +1,35 @@
+es_igual_que = 5 == 5
+print(es_igual_que)
+es_diferente = 5 != 6
+print(es_diferente)
+es_mayor_que = 5 > 6
+print(es_mayor_que)
+es_mayor_igual_que = 5 >= 6
+print(es_mayor_igual_que)
+es_menor_que = 5 < 6
+print(es_menor_que)
+es_menor_igual_que = 5 <= 6
+print(es_menor_igual_que)
+#calculos combinados 
+a = 5
+b = 10 
+c = 20
+comparacion = a + b == c
+print(comparacion)
+#ejercicio 1 comparacion 
+novia = 1993
+cheri = 1993
+#comparar usuarior
+contrasena_almacenada = "123456"
+contrasena_escrita = "123456"
+print(contrasena_almacenada == contrasena_escrita)
+
+contrasena_almacenada = "123456"
+contrasena_escrita = '123456'
+print(contrasena_almacenada == contrasena_escrita)
+
+contrasena_almacenada = "123456"
+contrasena_escrita = '''123456'''
+print(contrasena_almacenada == contrasena_escrita)
+
+
